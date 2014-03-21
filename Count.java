4010fd41bd7485d4d4aefa6a28d320f75f9d8a85
Olabenjo Tunde <t.visualb@hotmail.com>
2014-03-21 17:13:33 +0200

    Added count to add all numbers displayed

--- Count.java
+++ Count.java
@@ -2,12 +2,13 @@
 public class Count {
 
 	public static void main(String[] args) {
-		
+		int count = 0;
 		for (int i = 1 ; i < 11 ; i++)
 		{
+			count+=i;
 			System.out.println(i);
 		}
-
+		System.out.println("Total= "+count);
 	}
 
 }
